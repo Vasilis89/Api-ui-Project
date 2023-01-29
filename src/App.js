693476773facf03f452f54b1c984454005e2a8a3
Vasilis Myrianthopoulos <vasilissubs89@gmail.com>
2023-01-29 18:48:55 -0500

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 function App() {
+  const [character, setCharacter] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  function apiCall() {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => {
+        setCharacter(data.results);
+      });
+  }
+  function handleClick() {
+    setIndex((prev) => {
+      if (prev < 19) {
+        return prev + 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  function back() {
+    setIndex((prev) => {
+      if (prev > 0) {
+        return prev - 1;
+      } else {
+        return 19;
+      }
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {character.length && (
+        <>
+          <h1>Ricky and Morty API BROH!!!!!</h1>
+          <h2>{character[index].name}</h2>
+          <div className="container">
+            <button className="Tic-Tac" onClick={back}>
+              <AiOutlineArrowLeft />
+            </button>
+            <img
+              className="animation"
+              src={character[index].image}
+              alt="bruh"
+            />
+            <button className="broh" onClick={handleClick}>
+              <AiOutlineArrowRight />
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
 export default App;
